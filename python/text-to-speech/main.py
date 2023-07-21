9c858b3f44c78bae2e5a649bc3a80bf41c559044
rubynguyen1510 <50710444+rubynguyen1510@users.noreply.github.com>
2023-07-21 22:40:26 +0000

    set up

---
+++ python/text-to-speech/main.py
@@ -0,0 +1,19 @@
+class text_to_speech:
+    def __init__(self, req):
+        pass
+    def aws(self, req):
+        pass
+    def google(self, req):
+        pass
+    def azure(self, req):
+        pass
+    def validate_request(self, req):
+        pass
+
+"""
+Input:
+    req (json): contains payload and variables
+        payload includes: provider, language, text
+"""
+def main(req,res):
+    pass
