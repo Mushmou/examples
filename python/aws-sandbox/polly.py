bcd77f633d112dfe256ffad45788cb1ec5667421
rubynguyen1510 <50710444+rubynguyen1510@users.noreply.github.com>
2023-07-21 22:35:42 +0000

    set up

--- python/aws-sandbox/polly.py
+++ python/aws-sandbox/polly.py
@@ -1,10 +1,3 @@
-"""
-export AWS_ACCESS_KEY_ID="AKIAXLJW4PA6GB5R7WM7",
-export AWS_SECRET_ACCESS_KEY="Hx3lrTmg+hMuo1yr3rV2OZT1S6CC/0ictjLhXRQX"
-export AWS_ACCESS_KEY_ID=your_access_key_id
-export AWS_SECRET_ACCESS_KEY=your_secret_access_key
-export AWS_DEFAULT_REGION=your_default_region
-"""
 import boto3
 
 # Specify the AWS region you want to use
@@ -21,7 +14,7 @@ def text_to_speech(text, output_format='mp3', voice_id='Joanna'):
     response = polly_client .synthesize_speech(
     OutputFormat='mp3',
     SampleRate='8000',
-    Text='Hello, Noah',
+    Text=text,
     TextType='text',
     VoiceId='Joanna',
 )
