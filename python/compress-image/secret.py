6467b93aff151815907b32bdf355ea3aa80c9415
Noah Jacinto <noahjacinto@Noahs-MacBook-Air.local>
2023-07-21 10:27:54 -0700

    removed compress-image

--- python/compress-image/secret.py
+++
@@ -1,8 +0,0 @@
-'''
-API Key for tinyPNG and KrakenIO are stored here
-
-You should be in python/compress-image directory to run test_main.py
-'''
-API_KEY_TINYPNG = None
-API_KEY_KRAKENIO = None
-SECRET_API_KEY_KRAKENIO = None
